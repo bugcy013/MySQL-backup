01f7d11d312ca833057200b604943283741a869c
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2014-06-11 18:48:43 +0500

    Finalizing code

--- dump_backup_script/dump.py
+++ dump_backup_script/dump.py
@@ -36,38 +36,6 @@ class MysqlDumper:
             os.makedirs(self.backupdir + '/dumps')
             print('Created')
 
-    def recent_full_backup_file(self):
-        # Return last full backup dir name
-
-        if len(os.listdir(self.full_dir)) > 0:
-            return max(os.listdir(self.full_dir))
-        else:
-            return False
-
-
-    def last_full_backup_date(self):
-        # Finding last full backup date from dir/folder name
-
-        max = self.recent_full_backup_file()
-        dir_date_str = max[:4] + '-' + max[5:7] + '-' + max[8:10] + ' ' + max[11:13] + ':' + max[14:16]
-        dir_date = datetime.strptime(dir_date_str, "%Y-%m-%d %H:%M")
-        now = datetime.now().replace(second=0, microsecond=0)
-
-        # Defining variables for comparison.
-
-        a = '2013-09-04 15:29'
-        b = '2013-09-03 15:29'
-        a = datetime.strptime(a, "%Y-%m-%d %H:%M")
-        b = datetime.strptime(b, "%Y-%m-%d %H:%M")
-        diff = a - b
-
-        # Finding if last full backup is 1 day or more from now!
-
-        if now - dir_date >= diff:
-            return True
-        else:
-            return False
-
 
     def clean_full_backup_dir(self):
         # Deleting full backup after taking new full backup
@@ -82,11 +50,30 @@ class MysqlDumper:
 
         # Defining file name based on datetime
         now = datetime.now().replace(second=0, microsecond=0)
-        date = now[:10] + '_' + now[11:16]
-        file_name = date
+        now = str(now)
+        date1 = now[:10] + '_' + now[11:16]
+        file_name = date1
 
         # Taking Full backup
-        command = '%s %s > %s/%s.sql' % (self.backup_tool, self.myuseroption, self.full_dir,file_name)
-        args = shlex.split(command)
-        fb = subprocess.Popen(args, stdout=subprocess.PIPE)
-        print(str(fb.stdout.read()))
+        command = '%s %s > %s/%s.sql' % (self.backup_tool, self.myuseroption, self.full_dir, file_name)
+        # args = shlex.split(command)
+        # fb = subprocess.Popen(args, stdout=subprocess.PIPE)
+        # print(str(fb.stdout.read()))
+
+        print(command)
+
+
+    def all_procedures(self):
+
+        # Calling Backup function
+
+        self.take_dump_backup()
+
+        # Cleaning old backup
+
+        self.clean_full_backup_dir()
+
+
+x = MysqlDumper()
+#x.all_procedures()
+x.take_dump_backup()
